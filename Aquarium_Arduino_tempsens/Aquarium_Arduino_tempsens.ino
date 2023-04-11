f31c4a363a5b25e1b45eaac51ca1b3880d7bd178
Claudio <claudio.lammirato@gmail.com>
2023-04-11 18:58:06 +0200

    v0.0

--- Aquarium_Arduino_tempsens/Aquarium_Arduino_tempsens.ino
+++ Aquarium_Arduino_tempsens/Aquarium_Arduino_tempsens.ino
@@ -2,6 +2,11 @@
 #include <WiFiNINA.h>
 #include <WiFiClient.h>
 #include <ArduinoOTA.h>
+#include "wiring_private.h"
+#include <iostream>
+#include <string>
+#include "wirelessXbee.h"
+
 
 
 //library for wifi Debug with putty
@@ -15,13 +20,34 @@ float external_temp;
 float external_hum;
 float internal_temp;
 
+//Arduino IOT 33 mySerial Initialization
+Uart mySerial (&sercom0, 5, 6, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+//Specifi Function for mySerial
+void SERCOM0_Handler()
+{
+    mySerial.IrqHandler();
+}
+
+//Adrress Low of Xbee tfor communication
+std::string addresslow = "407C6019";
+
+
+
+
 void setup() {
   
   //Initialize serial:
   TelnetStream.begin();
+  //Serial for Xbee
+  mySerial.begin(9600);
+  Serial.begin(9600);
+
+  // Reassign pins 5 and 6 to SERCOM alt
+  pinPeripheral(5, PIO_SERCOM_ALT);
+  pinPeripheral(6, PIO_SERCOM_ALT);
 
   // start the WiFi OTA library with internal (flash) based storage
-  ArduinoOTA.begin(WiFi.localIP(), "Arduino", "claudio", InternalStorage);
+  ArduinoOTA.begin(WiFi.localIP(), "Arduino", "password", InternalStorage);
 }
 
 void loop() {
@@ -30,7 +56,7 @@ void loop() {
   ArduinoOTA.poll();
   //Telnet message
   static unsigned long next;
-  if (millis() - next > 600000) {
+  if (millis() - next > 10000) {
     next = millis();
     external_temp = retrievetemp();
     external_hum = retrievehum();
@@ -38,8 +64,38 @@ void loop() {
 
     //print Wifi Status
     printwifistatus();
-    //Print external temp and Hum
+
+    //Print external temp and Hum - wifi library and save it to ThingsBoard
     printext(external_temp, external_hum,internal_temp);
     thingsb_aquarium_send(external_temp,external_hum,internal_temp);
+
+    std::string msg_string="Claudio_" + std::to_string(external_temp);
+    std::string msg_hex;
+    std::string msg_out;
+
+    //Convert to HEX
+    stream2hex(msg_string, msg_hex, true);
+    writeXbee(msg_hex,msg_out, addresslow);
+    byte ans[50];
+    int hexNumber;
+
+    Serial.println(msg_out.length()/2);
+    for (int i=0;i<msg_out.length()/2;i+=2){
+      sscanf(msg_out.substr(i, 2).c_str(), "%x", &hexNumber);
+      ans[i/2] = hexNumber;
+    }
+    
+    
+
+    //mySerial.write(num);
+    //Serial.println(ans);
+    Serial.println(msg_out.c_str());
+    Serial.println(sizeof(ans) / sizeof(int));
+    byte xxx[] = {0x7E, 0x00, 0X15, 0x10, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x7C, 0x60, 0x19, 0xFF, 0xFE, 0x00, 0x00, 0x43, 0x6C, 0x61, 0x75, 0x64, 0x69, 0x6F, 0x46};
+    Serial.write(xxx, 35);
+    Serial.println(msg_string.c_str());
+
+
+
   }
 }
---
+++ Aquarium_Arduino_tempsens/wirelessXbee.cpp
@@ -0,0 +1,168 @@
+#include "wirelessXbee.h"
+
+void writeXbee(std::string str, std::string& msg_out, std::string addresslow){
+    //std::cout << str<<'\n';
+    //std::cout << str.length()<< '\n';
+    //hexstr = str + "_OUT";
+
+    std::string checksum;
+    int l;
+
+    //0 byte
+    msg_out = msg_out + '7'; // Start Delimeter
+    msg_out = msg_out + 'E'; // Start Delimeter
+    //1 byte
+    msg_out = msg_out + '0'; // Plceholder length
+    msg_out = msg_out + '0'; // Plceholder length
+    //2 byte placeholder - at the end will be calculated the lenght in hex
+    msg_out = msg_out + '0'; // Plceholder length
+    msg_out = msg_out + '0'; /// Plceholder length
+    //3 byte
+    msg_out = msg_out + '1'; // Frame Type
+    msg_out = msg_out + '0'; // Frame Type
+    //4 byte
+    msg_out = msg_out + '0'; //Frame ID
+    msg_out = msg_out + '1'; //Frame ID
+    //5 byte
+    msg_out = msg_out + '0'; //Address HIGH
+    msg_out = msg_out + '0'; //Address HIGH
+    //6 byte
+    msg_out = msg_out + '1'; //Address HIGH
+    msg_out = msg_out + '3'; //Address HIGH
+    //7 byte
+    msg_out = msg_out + 'A'; //Address HIGH
+    msg_out = msg_out + '2'; //Address HIGH
+    //8 byte
+    msg_out = msg_out + '0'; //Address HIGH
+    msg_out = msg_out + '0'; //Address HIGH
+    
+    //from 9 to 12 byte - Address LOW
+    for(int i = 0; i<addresslow.length(); i++){
+        msg_out = msg_out + addresslow[i];
+    }
+
+    //13 byte
+    msg_out = msg_out + 'F'; //16 Bit Destination address
+    msg_out = msg_out + 'F'; //16 Bit Destination address
+    //14 byte
+    msg_out = msg_out + 'F'; //Broadcast Radius + Options
+    msg_out = msg_out + 'E'; //Broadcast Radius + Options
+    //15 byte
+    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; // Start Delimeter
+    //16 byte
+    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; // Start Delimeter
+    
+   //message byte
+    msg_out = msg_out + str; // Start Delimeter
+    
+    //Checksum Calculation
+    for(int i = 6; i<34 + str.length() ; i+=2 ){
+        checksum = hexadd(checksum,msg_out.substr(i, 2));//checksum        
+    }
+    std::cout << checksum<<'\n';
+    if (checksum.length()>2){
+        checksum = checksum.substr(1,2); 
+    }else{
+        checksum = checksum.substr(0,2); 
+    }
+    std::cout << checksum<<'\n';
+    
+    checksum = hexand(checksum, "FF");
+    std::cout << checksum<<'\n';
+    checksum = hexmin("FF" , checksum);
+
+    if (checksum.length()<2){
+        checksum.insert(0, "0");
+    }
+    std::cout << checksum<<'\n';
+
+    msg_out = msg_out + checksum; // Checksum
+    
+    //Length calculation
+    l = (msg_out.length()-8)/2;
+    //std::cout << typeid(l).name()<<'\n';
+    std::stringstream stream;
+    stream << std::uppercase << std::hex << l;
+    std::string result( stream.str() );
+    //std::cout << result <<'\n';
+
+    msg_out[4] = result[0]; // Change with right length
+    msg_out[5] = result[1]; // Change with right length
+}
+
+
+// Convert string of chars to its representative string of hex numbers
+void stream2hex(const std::string str, std::string& hexstr, bool capital)
+{
+    hexstr.resize(str.size() * 2);
+    const size_t a = capital ? 'A' - 1 : 'a' - 1;
+
+    for (size_t i = 0, c = str[0] & 0xFF; i < hexstr.size(); c = str[i / 2] & 0xFF)
+    {
+
+        hexstr[i++] = c > 0x9F ? (c / 16 - 9) | a : c / 16 | '0';
+        hexstr[i++] = (c & 0xF) > 9 ? (c % 16 - 9) | a : c % 16 | '0';
+    }
+}
+
+
+// Convert string of hex numbers to its equivalent char-stream
+void hex2stream(const std::string hexstr, std::string& str)
+{
+    str.resize((hexstr.size() + 1) / 2);
+
+    for (size_t i = 0, j = 0; i < str.size(); i++, j++)
+    {
+        str[i] = (hexstr[j] & '@' ? hexstr[j] + 9 : hexstr[j]) << 4, j++;
+        str[i] |= (hexstr[j] & '@' ? hexstr[j] + 9 : hexstr[j]) & 0xF;
+    }
+}
+
+
+std::string hexadd(const std::string & hex1, const std::string & hex2)
+{
+	long n1, n2;
+	n1 = hex2dec(hex1);
+	n2 = hex2dec(hex2);
+	return dec2hex(n1+n2);
+}
+
+std::string hexmin(const std::string & hex1, const std::string & hex2)
+{
+	long n1, n2;
+	n1 = hex2dec(hex1);
+	n2 = hex2dec(hex2);
+	return dec2hex(n1-n2);
+}
+
+std::string hexand(const std::string & hex1, const std::string & hex2)
+{
+	long n1, n2;
+	n1 = hex2dec(hex1);
+	n2 = hex2dec(hex2);
+	return dec2hex(n1&n2);
+}
+
+std::string dec2hex(long i)
+{
+	std::stringstream ss;
+	std::string s;
+	hex(ss);
+	uppercase(ss);
+	ss << i;
+	ss >> s;
+	return s;
+}
+
+long hex2dec(const std::string & hexstr)
+{
+	std::stringstream ss;
+	long i = 0;
+	hex(ss);
+	ss << hexstr;
+	ss.clear();
+	ss >> i;
+	return i;
+}
